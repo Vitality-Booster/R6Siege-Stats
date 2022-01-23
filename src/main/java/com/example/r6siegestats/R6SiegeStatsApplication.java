3712397ffc2c4bae20297b7986a8a2171906967e
Vitali Bestolkau <bestolkovv@gmail.com>
2022-01-23 12:27:05 +0100

    Initial commit

---
+++ src/main/java/com/example/r6siegestats/R6SiegeStatsApplication.java
@@ -0,0 +1,13 @@
+package com.example.r6siegestats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R6SiegeStatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(R6SiegeStatsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/r6siegestats/R6SiegeStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.r6siegestats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class R6SiegeStatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
